bb795ff40f76587552e6b9e35da16646ff310ccf
Raditya Kertiyasa <adit.kerti@gmail.com>
2014-07-22 09:24:14 +0700

    Close #6
    - Made sure all text fields select all text on focus
    - Implemented single-click edit on all editable datagrids
    - Reformatted XAML for more comfortable code editing

--- pf-analyzer/pf-analyzer/MainWindow.xaml.cs
+++ pf-analyzer/pf-analyzer/MainWindow.xaml.cs
@@ -99,6 +99,43 @@ namespace pf_analyzer
             }
         }
 
+        private void DataGrid_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Lookup for the source to be DataGridCell
+            if (e.OriginalSource.GetType() == typeof(DataGridCell))
+            {
+                // Starts the Edit on the row;
+                DataGrid grd = (DataGrid)sender;
+                grd.BeginEdit(e);
+
+                Control control = GetFirstChildByType<Control>(e.OriginalSource as DataGridCell);
+                if (control != null)
+                {
+                    control.Focus();
+                }
+            }
+        }
+
+        private T GetFirstChildByType<T>(DependencyObject prop) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(prop); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild((prop), i) as DependencyObject;
+                if (child == null)
+                    continue;
+
+                T castedProp = child as T;
+                if (castedProp != null)
+                    return castedProp;
+
+                castedProp = GetFirstChildByType<T>(child);
+
+                if (castedProp != null)
+                    return castedProp;
+            }
+            return null;
+        }
+
         #endregion Common Control Events
 
         #region Window Drag and Drop Events
